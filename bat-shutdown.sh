83e4d927854fdd200afadac882e2b9598cab1628
anomalous-creator <christopher.scott.parks@gmail.com>
2022-02-12 13:38:22 -0600

    removed redundant

--- bat-shutdown.sh
+++ bat-shutdown.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
 
-min=5
+MIN=5
 while true
 do
     state=$(acpi | cut -d, -f 1 | cut -c 12-14)
     if [ $state = "Dis" ]
     then
-        bat=$(acpi | cut -d, -f 2 | cut -c 1-3 | sed 's/[%,]//g')
-        if [ "$bat" -le "$min" ]
+        bat=$(acpi | cut -d, -f 2 | sed 's/[%,]//g')
+        if [ "$bat" -le "$MIN" ]
         then
-            echo $bat
             ZZZ
         fi
     fi
-    sleep 10
 done
